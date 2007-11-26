9ddb3ba41e5d8cee41173e4fd102affbc7c93973
Federico Lucifredi <flucifredi@acm.org>
2007-11-26 00:18:59 +0000

    basic script to redeploy a modified server development instance.
    git-svn-id: file:///home/lennart/svn/public/avahi/branches/federico@1572 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

---
+++ redeploy.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# $Id: redeploy.sh 1548 2007-10-06 23:47:44Z federico $
+
+# This file is part of avahi.
+#
+# avahi is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# avahi is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with avahi; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+
+make all
+echo "===== make all complete ====="
+make install
+echo "===== make install complete ====="
+ldconfig
+/etc/init.d/messagebus restart
+echo "===== ldconfig, dbus re-init'd ====="
