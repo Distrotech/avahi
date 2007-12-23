4d7097e322fe68cd530d3199aa4a21308919fa84
Federico Lucifredi <flucifredi@acm.org>
2007-12-23 02:01:07 +0000

    syntax terror.
    git-svn-id: file:///home/lennart/svn/public/avahi/branches/federico@1607 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-core/rr.h
+++ avahi-core/rr.h
@@ -46,9 +46,9 @@ enum {
 /** TSIG signature algorithms, defined in RFCs 2845 and 4635 */
 
 enum {
-    AVAHI_TSIG_HMAC_MD5 = 0x1;
-    AVAHI_TSIG_HMAC_SHA1 = 0x2;
-    AVAHI_TSIG_HMAC_SHA256 = 0x3;
+    AVAHI_TSIG_HMAC_MD5 = 0x1,
+    AVAHI_TSIG_HMAC_SHA1 = 0x2,
+    AVAHI_TSIG_HMAC_SHA256 = 0x3,
 };
 
 /** DNS record classes, see RFC 1035, in addition to those defined in defs.h */
