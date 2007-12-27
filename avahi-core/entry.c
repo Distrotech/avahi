62fdc0229e6a24040d9e3c29ce238371f0e9660b
Federico Lucifredi <flucifredi@acm.org>
2007-12-27 05:04:51 +0000

    hah. Pascal-style string, what a deja-vu.
    git-svn-id: file:///home/lennart/svn/public/avahi/branches/federico@1638 941a03a8-eaeb-0310-b9a0-b1bbd8fe43fe

--- avahi-core/entry.c
+++ avahi-core/entry.c
@@ -225,7 +225,7 @@ static AvahiEntry * server_add_internal(
     /* may have to move lower into logic, for _new_ and _modified_ records only */
     printf("+++ record of type %d named %s observed at entrypoint\n", r->key->type, r->key->name);
 
-    char[100] out;
+    char out[100];
     int out_len = 100;
     avahi_escape_label("foo.com", 8, &out, &out_len);
     printf("result: -%s-\n", out);
